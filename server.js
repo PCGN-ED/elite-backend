59f9fc495c6002495dca0f4c09d714516e6c3a40
Michael Tesluck <mtesluck@outlook.com>
2025-04-18 21:19:26 -0400

    Force rebuild to fix DB connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 const express = require('express');
 const cors = require('cors');
